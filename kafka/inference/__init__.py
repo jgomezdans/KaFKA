a250c68f305019336ec619a92a7442072f6649ae
José Gómez-Dans <jgomezdans@gmail.com>
2018-01-09 14:18:22 +0000

    Fixed typo

--- kafka/inference/__init__.py
+++ kafka/inference/__init__.py
@@ -2,5 +2,5 @@ __all__ = ['kf_tools', 'linear_kf', 'solvers', 'utils']
 # deprecated to keep older scripts who import this from breaking
 from .inference.kf_tools import *
 from .inference.linear_kf import *
-from .inference.solvers imoprt *
+from .inference.solvers import *
 from .inference.utils import *
