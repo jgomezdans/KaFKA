a4197053e8dd14a626b70ec6078b826d4cd0171c
Nicola Pounder <nicola.pounder@assimila.eu>
2018-01-17 13:15:21 +0000

    Create setup.py
    requirements might need expanding.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+requirements = [
+    'pytest',
+    'numpy',
+    'scipy'
+]
+
+setup(name='multiply-KaFKA-inference-engine',
+      description='MULTIPLY KaFKA inference engine',
+      author='MULTIPLY Team',
+      packages=['kafka',
+                 'inference',
+                 'input_output'],
+      install_requires=requirements
+)
