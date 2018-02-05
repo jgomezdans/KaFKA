c63d5d86175b7f7989f9396aa0b5e073bb7a4f58
José Gómez-Dans <jgomezdans@gmail.com>
2018-02-05 13:26:38 +0000

    Added S1 observations

--- kafka/input_output/__init__.py
+++ kafka/input_output/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["observations", "Sentinel2_Observations"]
+__all__ = ["observations", "Sentinel1_Observations", "Sentinel2_Observations"]
 
 from .observations import *
+from .Sentinel1_Observations import S1Observations
 from .Sentinel2_Observations import Sentinel2Observations
