245c3f35fc0024aa3fd909b304a9aa2e1e21ca4b
José Gómez-Dans <jgomezdans@gmail.com>
2018-09-25 18:21:19 +0100

    update

--- kafka_test_Py36.py
+++ kafka_test_Py36.py
@@ -42,62 +42,7 @@ from kafka.state_propagation import IdentityPropagator, NoPropagator
     ###def process_prior(self, parameters: List[str], time: Union[str, datetime], state_grid: np.array,
 
 
-class JRCPrior(object):
-    """Dummpy 2.7/3.6 prior class following the same interface as 3.6 only
-    version."""
 
-    def __init__ (self, parameter_list, state_mask):
-        """It makes sense to have the list of parameters and state mask
-        defined at this point, as they won't change during processing."""
-        self.mean, self.covar, self.inv_covar = self._tip_prior() 
-        self.parameter_list = parameter_list
-        if isinstance(state_mask, (np.ndarray, np.generic) ):
-            self.state_mask = state_mask
-        else:
-            self.state_mask = self._read_mask(state_mask)
-            
-    def _read_mask(self, fname):
-        """Tries to read the mask as a GDAL dataset"""
-        if not os.path.exists(fname):
-            raise IOError("State mask is neither an array or a file that exists!")
-        g = gdal.Open(fname)
-        if g is None:
-            raise IOError("{:s} can't be opened with GDAL!".format(fname))
-        mask = g.ReadAsArray()
-        return mask
-
-    def _tip_prior(self):
-        """The JRC-TIP prior in a convenient function which is fun for the whole
-        family. Note that the effective LAI is here defined in transformed space
-        where TLAI = exp(-0.5*LAIe).
-
-        Returns
-        -------
-        The mean prior vector, covariance and inverse covariance matrices."""
-        # broadly TLAI 0->7 for 1sigma
-        sigma = np.array([0.12, 0.7, 0.0959, 0.15, 1.5, 0.2, 0.6])
-        x0 = np.array([0.17, 1.0, 0.1, 0.7, 2.0, 0.18, np.exp(-0.5*2.)])
-        # The individual covariance matrix
-        little_p = np.diag(sigma**2).astype(np.float32)
-        little_p[5, 2] = 0.8862*0.0959*0.2
-        little_p[2, 5] = 0.8862*0.0959*0.2
-
-        inv_p = np.linalg.inv(little_p)
-        return x0, little_p, inv_p
-
-    def process_prior ( self, time, inv_cov=True):
-        # Presumably, self._inference_prior has some method to retrieve 
-        # a bunch of files for a given date...
-        n_pixels = self.state_mask.sum()
-        x0 = np.array([self.mean for i in range(n_pixels)]).flatten()
-        if inv_cov:
-            inv_covar_list = [self.inv_covar for m in range(n_pixels)]
-            inv_covar = block_diag(inv_covar_list, dtype=np.float32)
-            return x0, inv_covar
-        else:
-            covar_list = [self.covar for m in range(n_pixels)]
-            covar = block_diag(covar_list, dtype=np.float32)
-            return x0, covar
         
 class KafkaOutputMemory(object):
     """A very simple class to output the state."""
