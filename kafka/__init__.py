666d43a803f56ccab2679748113e7435cca7fa78
Jose Gomez-Dans <jgomezdans@gmail.com>
2018-09-25 18:53:30 +0100

    Added version to main __init__

--- kafka/__init__.py
+++ kafka/__init__.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+__author__ = "J Gomez-Dans"
+__copyright__ = "Copyright 2017, 2018 J Gomez-Dans"
+__version__ = "0.5.0"
+__license__ = "GPLv3"
+__email__ = "j.gomez-dans@ucl.ac.uk"
+
+
 from .inference import *
 from .input_output import *
 from .linear_kf import LinearKalman
--- setup.py
+++ setup.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python
+import codecs
+import os
+import re
 
 from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 requirements = [
     'pytest',
     'numpy',
     'scipy',
+    'gp_emulator',
+    'BRDF_descriptors',
     'gdal',
     # 'BRDF_descriptors', # Not available for automatic installation
     'matplotlib'
 ]
 
 setup(name='KaFKA',
+      version=find_version("kafka", "__init__.py"),
       description='MULTIPLY KaFKA inference engine',
-      author='MULTIPLY Team',
+      author='J Gomez-Dans',
+      author_email='j.gomez-dans@ucl.ac.uk',
+      url='https://github.com/jgomezdans/KaFKA',
       packages=find_packages(),
       install_requires=requirements
 )
