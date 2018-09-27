7bdaf093fd774368e68602da94e804662ad1f83e
Jose Gomez-Dans <jgomezdans@gmail.com>
2018-09-27 18:35:42 +0100

    Added some tests
    Added some propagation tests. They need to be a bit more thorough.
    
    I have also updated `.travis.yml` and removed some legacy options,
    like python 2.7 and so on.

--- tests/test_state_propagation.py
+++ tests/test_state_propagation.py
@@ -9,8 +9,19 @@ from pytest import fixture
 from distutils import dir_util      
 
 from kafka import NoPropagator, IdentityPropagator
+from kafka import forward_state_propagation
 
-
+class TestPrior(object):
+    
+    def __init__(self, n_params, n_pxls):
+        self.n_params = n_params
+        self.n_pxls = n_pxls
+    
+    def process_prior(self, time_step):
+        x = np.ones(self.n_params*self.n_pxls)*5.
+        inv_covar = sp.eye(self.n_params*self.n_pxls)*50.
+        return x, inv_covar
+        
 
 
 def test_nopropagation_m():
@@ -60,3 +71,38 @@ def test_identitypropagation_q():
                                 None, None, None)
     assert np.allclose(spl.norm(Q_true), 
                        spl.norm(Q)) 
+
+def test_no_propagation_mean():
+    #forward_state_propagation(x_analysis, P_analysis, P_analysis_inv,
+                              #M_matrix, Q_matrix, 
+                              #time_step, prior_obj):
+    prior_obj = TestPrior(7, 1)
+    xa = np.zeros(7) # Say
+    P_analysis = None
+    P_analysis_inv = sp.eye(7)
+    P_analysis_inv.setdiag(np.ones(7)*100.)
+    M_matrix = sp.eye(7)*0.
+    Q_matrix = sp.eye(7)*0.
+    time_step = None 
+    xf, Pa, Pa_inv = forward_state_propagation(xa, P_analysis, P_analysis_inv,
+                              M_matrix, Q_matrix, 
+                              time_step, prior_obj)
+    assert np.allclose(xf, np.ones(7)*5.)
+    
+def test_no_propagation_invcov():
+    #forward_state_propagation(x_analysis, P_analysis, P_analysis_inv,
+                              #M_matrix, Q_matrix, 
+                              #time_step, prior_obj):
+    prior_obj = TestPrior(7, 1)
+    xa = np.zeros(7) # Say
+    P_analysis = None
+    P_analysis_inv = sp.eye(7)
+    P_analysis_inv.setdiag(np.ones(7)*100.)
+    M_matrix = sp.eye(7)*0.
+    Q_matrix = sp.eye(7)*0.
+    time_step = None 
+    xf, Pa, Pa_inv = forward_state_propagation(xa, P_analysis, P_analysis_inv,
+                              M_matrix, Q_matrix, 
+                              time_step, prior_obj)
+    assert np.allclose(np.array( (sp.eye(7)*50.).todense()),
+                       np.array( (Pa_inv).todense()))
