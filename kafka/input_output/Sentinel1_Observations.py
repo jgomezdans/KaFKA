23c4fd026f49ebd1c1bb91c25a5f3995619b6314
José Gómez-Dans <jgomezdans@gmail.com>
2018-11-19 15:27:08 +0000

    Updated to follow LMU's new SAR format

--- kafka/input_output/Sentinel1_Observations.py
+++ kafka/input_output/Sentinel1_Observations.py
@@ -168,7 +168,8 @@ class S1Observations(object):
         elif band == 1:
             polarisation = 'VH'
         this_file = self.date_data[timestep]
-        fname = 'NETCDF:"{:s}":sigma0_{:s}'.format(this_file, polarisation)
+        fname = 'NETCDF:"{:s}":sigma0_{:s}_multi'.format(this_file,
+                                                         polarisation.lower())
         obs_ptr = reproject_image(fname, self.state_mask)
         observations = self._read_backscatter(obs_ptr)
         uncertainty = self._calculate_uncertainty(observations)
@@ -192,5 +193,8 @@ class S1Observations(object):
 
 
 if __name__ == "__main__":
-    data_folder = "/media/ucfajlg/WERKLY/jose/new/"
-    sentinel1 = S1Observations(data_folder)
+    data_folder = "/data/selene/ucfajlg/ELBARA_LMU/LMU_SAR_testcase"
+    state_mask = "/home/ucfajlg/Data/python/LMU_testcase/data/ESU.tif"
+    s1_observations = S1Observations(data_folder, state_mask)
+    for timestep in s1_observations.dates:
+        retval = s1_observations.get_band_data(timestep, 0)
